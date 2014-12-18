17f490b1181c7f2a3849496d4da0d1cdd4bb2b9b
Xiaoxu <xiaoxul@andrew.cmu.edu>
2014-12-18 02:30:37 -0500

    eleventh

---
+++ src/levelOrderBottom/Solution.java
@@ -0,0 +1,59 @@
+package levelOrderBottom;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+import util.TreeNode;
+
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+    	List<Integer> inner = new ArrayList<Integer>();
+    	Queue<TreeNode> cq = new LinkedList<TreeNode>();
+    	Queue<TreeNode> nq = new LinkedList<TreeNode>();
+    	Stack<List<Integer>> s = new Stack<List<Integer>>();
+    	
+    	if(root == null)
+    		return res;
+    	cq.add(root);
+    	while(!cq.isEmpty()){
+    		TreeNode t = cq.poll();
+    		inner.add(t.val);
+    	
+    		if(t.left != null)
+    			nq.add(t.left);
+    		if(t.right != null)
+    			nq.add(t.right);
+    		if(cq.isEmpty()){
+    			s.push(new ArrayList<Integer> (inner));
+    			inner.clear();
+    			cq.addAll(nq);
+    			nq.clear();
+    		}
+    	}
+    	//one more stack process than level order
+    	while(!s.isEmpty()){
+    		List<Integer> l = s.pop();
+    		res.add(new ArrayList<Integer> (l));
+    	}
+        return res;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution test = new Solution();
+		TreeNode root = new TreeNode(3);
+		TreeNode a = new TreeNode(5);
+		TreeNode b = new TreeNode(7);
+		TreeNode c = new TreeNode(8);
+		TreeNode d = new TreeNode(9);
+		root.left = a;
+		root.right = b;
+		a.right = c;
+		b.right = d;
+		System.out.println(test.levelOrderBottom(root));
+	}
+
+}
--- src/util/TreeNode.java
+++ src/util/TreeNode.java
@@ -1,5 +1,9 @@
 package util;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
 	public int val;
 	public TreeNode left;
@@ -9,9 +13,57 @@ public class TreeNode {
 		val = x;
 	}
 	
-//	public String toString() {
-//		String s = "";
-//		// waiting~
-//		return s;
-//	}
+	public String toString() {
+		String s = "";
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		TreeNode root = this;
+		q.add(root);
+		while(!q.isEmpty()){
+			TreeNode t = q.poll();
+			s += t.val + " ";
+			if(t.left != null)
+				q.add(t.left);
+			if(t.right != null)
+				q.add(t.right);
+		}
+		
+		return s;
+	}
+	public String toStringlevel(){
+		String s = "";
+		Queue<TreeNode> c = new LinkedList<TreeNode>();
+		Queue<TreeNode> n = new LinkedList<TreeNode>();
+		TreeNode root = this;
+		c.add(root);
+		while(!c.isEmpty()){
+			TreeNode t = c.poll();
+			s += t.val + " ";
+			if(t.left != null)
+				n .add(t.left);
+			if(t.right != null)
+				n.add(t.right);
+			if(c.isEmpty()){
+				s += "\n";
+				c.addAll(n);
+				n.clear();
+			}
+		}
+		
+		return s;
+	}
+	public static void main(String[] args){
+		TreeNode root = new TreeNode(3);
+		TreeNode a = new TreeNode(5);
+		TreeNode b = new TreeNode(7);
+		TreeNode c = new TreeNode(8);
+		TreeNode d = new TreeNode(9);
+		root.left = a;
+		root.right = b;
+		a.right = c;
+		b.right = d;
+		System.out.println(root.toStringlevel());
+		System.out.println(root);
+			
+	}
 }
+
