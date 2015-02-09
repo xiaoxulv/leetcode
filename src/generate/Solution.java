15fed583b27c0a3b1b2dc449d63d549f8289522e
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-02-08 22:49:38 -0500

    Pascal Triangle

---
+++ src/generate/Solution.java
@@ -0,0 +1,28 @@
+package generate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        for(int i = 0; i < numRows; i++){
+        	ArrayList<Integer> in = new ArrayList<Integer>();
+        	in.add(1);
+        	if(i != 0){
+        		for(int j = 0; j < i-1; j++)
+        			in.add(res.get(i-1).get(j)+res.get(i-1).get(j+1));
+        		in.add(1);
+        	}
+        	res.add(in);
+        }
+        return res;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution test = new Solution();
+		System.out.println(test.generate(5));
+	}
+
+}
--- src/isSymmetric/Solution.java
+++ src/isSymmetric/Solution.java
@@ -3,19 +3,26 @@ package isSymmetric;
 import util.TreeNode;
 
 public class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        	if(root == null) return true;
-        	if(root.left == null && root.right == null)
-        		return true;
-        	if(root.left == null && root.right != null)
-        		return false;
-        	if(root.left != null && root.right == null)
-        		return false;
-        	if((root.left.left == root.right.right) &&
-        			(root.left.right == root.right.left))
-        		return true;
-        	return isSymmetric(root.left)&&isSymmetric(root.right);		
-    }
+	public boolean isSymmetric(TreeNode root) {
+		if (root == null)
+			return true;
+		return helper(root.left, root.right);
+	}
+
+	public boolean helper(TreeNode left, TreeNode right) {
+		if (left == null)
+			return right == null;
+		if(right == null)
+			return false;
+		if(left.val != right.val)
+			return false;
+		if(!helper(left.left, right.right))
+			return false;
+		if(!helper(left.right, right.left))
+			return false;
+		return true;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution test = new Solution();
